733168cef5252549f4ad464b844ba2e4b943156d
Hoss Shakiba <hossshakiba@outlook.com>
2024-03-26 13:02:25 +0330

    Change something

--- script/publications.js
+++ script/publications.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
               </p>
             </div>
             <p class="jurnal">
-            ${val.jurnal}
+            ${val.journal}, ${val.year}
             </p>
             <p class="author">
             ${val.author}
